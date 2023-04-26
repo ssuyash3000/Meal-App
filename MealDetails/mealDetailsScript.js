30561de52b93729b8b0a6c650a72a0d654f3201b
ssuyash3000 <ssuyash2001@gmail.com>
2023-04-27 05:09:07 +0530

    Implemented IIFE module pattern

--- MealDetails/mealDetailsScript.js
+++ MealDetails/mealDetailsScript.js
@@ -1,80 +1,84 @@
-let mealId = JSON.parse(localStorage.getItem("mealId"));
-//console.log(mealId + "HELLO");
-let container = document.getElementById("container");
-let mealName = document.getElementById("mealName");
-let photo = document.getElementById("photo");
-let mealType = document.getElementById("mealType");
-let mealCategory = document.getElementById("mealCategory");
-let instruction = document.getElementById("instruction");
-let ingridents = document.getElementById("ingridents");
-let source = document.getElementById("source");
+var movDetails = (function(){
+    let mealId = JSON.parse(localStorage.getItem("mealId"));
+    //console.log(mealId + "HELLO");
+    let container = document.getElementById("container");
+    let mealName = document.getElementById("mealName");
+    let photo = document.getElementById("photo");
+    let mealType = document.getElementById("mealType");
+    let mealCategory = document.getElementById("mealCategory");
+    let instruction = document.getElementById("instruction");
+    let ingridents = document.getElementById("ingridents");
+    let source = document.getElementById("source");
 
-async function fetchMovieDetails(){
+    async function fetchMovieDetails(){
+            
+        const response = await fetch(`https://themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+        meal = await response.json();
+        meal = meal.meals[0];
         
-    const response = await fetch(`https://themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
-    meal = await response.json();
-    meal = meal.meals[0];
-    
-    document.title = `${meal.strMeal}`;
+        document.title = `${meal.strMeal}`;
 
-    mealName.innerHTML =
-     `
-    <h2>Meal Name - ${meal.strMeal}</h2>
-    `;
+        mealName.innerHTML =
+        `
+        <h2>Meal Name - ${meal.strMeal}</h2>
+        `;
 
-    photo.innerHTML = 
-    `
-    <h3>Meal Photo - </h3>
-    <img src="${meal.strMealThumb}">
-    `;
+        photo.innerHTML = 
+        `
+        <h3>Meal Photo - </h3>
+        <img src="${meal.strMealThumb}">
+        `;
 
-    mealType.innerHTML = 
-    `
-    <h3>Meal Type - </h3>
-    <h2>${meal.strArea}</h2>
-    `;
+        mealType.innerHTML = 
+        `
+        <h3>Meal Type - </h3>
+        <h2>${meal.strArea}</h2>
+        `;
 
-    mealCategory.innerHTML=
-    `
-    <h3>Meal Category - </h3>
-    <h2>${meal.strCategory}</h2>
-    `;
+        mealCategory.innerHTML=
+        `
+        <h3>Meal Category - </h3>
+        <h2>${meal.strCategory}</h2>
+        `;
 
-    const recipeString = meal.strInstructions;
-    const recipeArray = recipeString.split("\r\n");
-    instruction.innerHTML = 
-    `
-    <h2 id="instructionLable">Instructions</h2>
-    `;
-    recipeArray.forEach(step => {
-        const p = document.createElement("p");
-        p.innerText = step;
-        instruction.appendChild(p);
-      });
+        const recipeString = meal.strInstructions;
+        const recipeArray = recipeString.split("\r\n");
+        instruction.innerHTML = 
+        `
+        <h2 id="instructionLable">Instructions</h2>
+        `;
+        recipeArray.forEach(step => {
+            const p = document.createElement("p");
+            p.innerText = step;
+            instruction.appendChild(p);
+        });
 
 
 
-    ingridents.innerHTML = 
-    `
-    <h3> List of Ingridents </h3>
+        ingridents.innerHTML = 
+        `
+        <h3> List of Ingridents </h3>
 
-    <ul>
-   <li> Ingridents 1 => ${meal.strIngredient1} </li>
-   <li> Ingridents 2 => ${meal.strIngredient2} </li>
-   <li> Ingridents 3 => ${meal.strIngredient3} </li>
-   <li> Ingridents 4 => ${meal.strIngredient4} </li>
-   <li> Ingridents 5 => ${meal.strIngredient5} </li>
-   <li> Ingridents 6 => ${meal.strIngredient6} </li>
-   <li> Ingridents 7 => ${meal.strIngredient7} </li>
-   <li> Ingridents 8 => ${meal.strIngredient8} </li>
-   <li> Ingridents 9 => ${meal.strIngredient9} </li>
-   <li> Ingridents 10 => ${meal.strIngredient10} </li>
-    <ul>
-    `;
+        <ul>
+            <li> Ingridents 1 => ${meal.strIngredient1} </li>
+            <li> Ingridents 2 => ${meal.strIngredient2} </li>
+            <li> Ingridents 3 => ${meal.strIngredient3} </li>
+            <li> Ingridents 4 => ${meal.strIngredient4} </li>
+            <li> Ingridents 5 => ${meal.strIngredient5} </li>
+            <li> Ingridents 6 => ${meal.strIngredient6} </li>
+            <li> Ingridents 7 => ${meal.strIngredient7} </li>
+            <li> Ingridents 8 => ${meal.strIngredient8} </li>
+            <li> Ingridents 9 => ${meal.strIngredient9} </li>
+            <li> Ingridents 10 => ${meal.strIngredient10} </li>
+        <ul>
+        `;
 
-    source.innerHTML = 
-    `
-        <a target="blank" href="${meal.strSource}" id = "sourceLink">Soruce of This information</a>
-    `;
-}
-fetchMovieDetails();
+        source.innerHTML = 
+        `
+            <a target="blank" href="${meal.strSource}" id = "sourceLink">Soruce of This information</a>
+        `;
+    }
+    return {
+        fetchMovieDetails:fetchMovieDetails,
+    };
+})();
--- script.js
+++ script.js
@@ -1,95 +1,101 @@
-let inputBar = document.getElementById("mealSearchBar");
-let listOfMealInDOM = document.getElementById("listOfMealInDOM");
-let errorMessage =  document.getElementById("errorMessage");
-let mealList = [];
-let favList = [];
-function addMealToDOM(meal){
-    let li = document.createElement("li");
-    li.dataset.id = `${meal.idMeal}`;
-    // li.setAttribute.id = "mealItem";
-    li.setAttribute("id", "mealItem");
-    li.innerHTML = `
-    <img src="${meal.strMealThumb}" id = "poster" data-id="${meal.idMeal}" >
-    <div id="details" data-id="${meal.idMeal}">
+var app =(function(){
+    let inputBar = document.getElementById("mealSearchBar");
+    let listOfMealInDOM = document.getElementById("listOfMealInDOM");
+    let errorMessage =  document.getElementById("errorMessage");
+    let mealList = [];
+    let favList = [];
+    function addMealToDOM(meal){
+        let li = document.createElement("li");
+        li.dataset.id = `${meal.idMeal}`;
+        // li.setAttribute.id = "mealItem";
+        li.setAttribute("id", "mealItem");
+        li.innerHTML = `
+        <img src="${meal.strMealThumb}" id = "poster" data-id="${meal.idMeal}" >
+        <div id="details" data-id="${meal.idMeal}">
 
-    <h1 id="mealTitle"  data-id="${meal.idMeal}">${meal.strMeal} </h1>
-  
-    <h3 id="mealCategory">Category - ${meal.strCategory}</h3>
-
-    <h3 id="area" data-id="${meal.idMeal}">Area - ${meal.strArea}</h3>
-
-    <a id="youtube" href="${meal.strYoutube}" target="blank"> Tutorial </a>
-
-    <a id="favBtn" data-id="${meal.idMeal}" data-title="${meal.strMeal}">Add to Favourites</a>
+        <h1 id="mealTitle"  data-id="${meal.idMeal}">${meal.strMeal} </h1>
     
-    </div>
-    `;
-    listOfMealInDOM.append(li);
-}
-async function searchTheMeal(text){
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`);
-    const data =  await response.json();
-    if(data.meals == null){
-        errorMessage.innerHTML = "No meals Found for given input";
-        listOfMealInDOM.innerHTML = "";
+        <h3 id="mealCategory">Category - ${meal.strCategory}</h3>
 
-    }else{
-        errorMessage.innerHTML = "";
-        mealList = data.meals;
-        listOfMealInDOM.innerHTML = "";
-        for(let i = 0; i < mealList.length; i++){
-            addMealToDOM(mealList[i]);
+        <h3 id="area" data-id="${meal.idMeal}">Area - ${meal.strArea}</h3>
+
+        <a id="youtube" href="${meal.strYoutube}" target="blank"> Tutorial </a>
+
+        <a id="favBtn" data-id="${meal.idMeal}" data-title="${meal.strMeal}">Add to Favourites</a>
+        
+        </div>
+        `;
+        listOfMealInDOM.append(li);
+    }
+    async function searchTheMeal(text){
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`);
+        const data =  await response.json();
+        if(data.meals == null){
+            errorMessage.innerHTML = "No meals Found for given input";
+            listOfMealInDOM.innerHTML = "";
+
+        }else{
+            errorMessage.innerHTML = "";
+            mealList = data.meals;
+            listOfMealInDOM.innerHTML = "";
+            for(let i = 0; i < mealList.length; i++){
+                addMealToDOM(mealList[i]);
+            }
+        }
+    }   
+    function handleKeys(e){
+        const text = inputBar.value;
+        if(text.length !== 0 && e.target.id  === "mealSearchBar"){
+            searchTheMeal(text);
         }
     }
-}   
-function handleKeys(e){
-    const text = inputBar.value;
-    if(text.length !== 0 && e.target.id  === "mealSearchBar"){
-        searchTheMeal(text);
+    function addToFav(mealId, mealTitle){
+        for(i of favList){
+            if(i===mealId){
+                errorMessage.innerHTML = "This meal is already in the Fav List";
+                setTimeout(()=>{
+                    errorMessage.innerHTML = "";
+                }, 4000);
+                
+                return;
+            }
+        }
+        favList.push(mealId);
+        errorMessage.innerHTML = `${mealTitle} added in the fav List`;
+        setTimeout(()=>{
+            errorMessage.innerHTML = "";
+        }, 4000);
+
+        //updating the favList array in the localStorage each time 
+        // favList is updated 
+        localStorage.setItem("favList", JSON.stringify(favList));
     }
-}
-function addToFav(mealId, mealTitle){
-    for(i of favList){
-        if(i===mealId){
-            errorMessage.innerHTML = "This meal is already in the Fav List";
-            setTimeout(()=>{
-                errorMessage.innerHTML = "";
-            }, 4000);
-            
-            return;
+    function handleClick(e){
+
+        if(e.target.id === "mealItem" || e.target.id === "poster" ||e.target.id === "details" 
+        || e.target.id ==="area" || e.target.id === "mealCategory" || e.target.id ==="mealTitle"){
+            let mealId = e.target.dataset.id;
+            localStorage.setItem("mealId", JSON.stringify(mealId));
+            //console.log("mealItem Clicked " + mealId + e.target.id);
+            window.open("./MealDetails/mealDetails.html");
+        }
+        else if(e.target.id === "clear"){
+            listOfMealInDOM.innerHTML = "";
+            errorMessage.innerHTML = "";
+            inputBar.value = "";
+        }
+        else if(e.target.id === "favourite"){
+            localStorage.setItem("mealId", JSON.stringify(favList));
+            window.open("./Favourite/favourite.html");
+        }else if(e.target.id === "favBtn"){
+            addToFav(e.target.dataset.id, e.target.dataset.title);
         }
     }
-    favList.push(mealId);
-    errorMessage.innerHTML = `${mealTitle} added in the fav List`;
-    setTimeout(()=>{
-        errorMessage.innerHTML = "";
-    }, 4000);
-
-    //updating the favList array in the localStorage each time 
-    // favList is updated 
-    localStorage.setItem("favList", JSON.stringify(favList));
-}
-function handleClick(e){
-
-    if(e.target.id === "mealItem" || e.target.id === "poster" ||e.target.id === "details" 
-    || e.target.id ==="area" || e.target.id === "mealCategory" || e.target.id ==="mealTitle"){
-        let mealId = e.target.dataset.id;
-        localStorage.setItem("mealId", JSON.stringify(mealId));
-        console.log("mealItem Clicked " + mealId + e.target.id);
-        window.open("./MealDetails/mealDetails.html");
+    function appInitalize(){
+        document.addEventListener("keyup", handleKeys);
+        document.addEventListener("click", handleClick);
     }
-    else if(e.target.id === "clear"){
-        listOfMealInDOM.innerHTML = "";
-        errorMessage.innerHTML = "";
-        inputBar.value = "";
-    }
-    else if(e.target.id === "favourite"){
-        localStorage.setItem("mealId", JSON.stringify(favList));
-        window.open("./Favourite/favourite.html");
-    }else if(e.target.id === "favBtn"){
-        addToFav(e.target.dataset.id, e.target.dataset.title);
-    }
-}
-
-document.addEventListener("keyup", handleKeys);
-document.addEventListener("click", handleClick);
+    return {
+        appInitalize:appInitalize,
+    };
+})();
